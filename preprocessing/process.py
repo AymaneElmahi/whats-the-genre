c7c83e5514beceabcd97843b625fba32e7999b8f
AymaneElmahi <elmahi.aymane@gmail.com>
2023-05-05 15:59:07 +0200

    update process

--- preprocessing/process.py
+++ preprocessing/process.py
@@ -88,13 +88,58 @@ def process_synopsis(df):
     return df
 
 
-def process_df(df,train = True):
+def process_df(df, train=True):
     if train:
         df = df.loc[:, ['synopsis', 'titre', 'genre']]
     else:
         df = df.loc[:, ['synopsis', 'titre']]
     df = process_title(df)
     df = process_synopsis(df)
+    if train:
+        df = df.loc[:, ['synopsis_use', 'titre_use', 'genre']]
+    else:
+        df = df.loc[:, ['synopsis_use', 'titre_use']]
+    # mix the synopsis and the title
+    df['synopsis_title'] = df['synopsis_use'] + ' ' + df['titre_use']
+    if train:
+        df = df.loc[:, ['synopsis_title', 'genre']]
+    else:
+        df = df.loc[:, ['synopsis_title']]
+    return df
+
+
+def process_title_transformer(df):
+    df['titre'] = df['titre'].str.lower()
+    df['titre'] = df['titre'].str.normalize('NFKD').str.encode(
+        'ascii', errors='ignore').str.decode('utf-8')
+    df['titre'] = df['titre'].str.translate(
+        str.maketrans('', '', string.punctuation))
+    df['titre_use'] = df['titre'].apply(lambda x: ' '.join(
+        [word for word in x.split() if word not in (stopwords)]))
+    # df['titre_use'] = df['titre'].apply(lemmatize_text)
+    return df
+
+
+def process_synopsis_transformer(df):
+    # remove_NER(df, 'synopsis')
+    df['synopsis'] = df['synopsis'].str.lower()
+    df['synopsis'] = df['synopsis'].str.normalize('NFKD').str.encode(
+        'ascii', errors='ignore').str.decode('utf-8')
+    df['synopsis'] = df['synopsis'].str.translate(
+        str.maketrans('', '', string.punctuation))
+    df['synopsis_use'] = df['synopsis'].apply(lambda x: ' '.join(
+        [word for word in x.split() if word not in (stopwords)]))
+    # df['synopsis_use'] = df['synopsis'].apply(lemmatize_text)
+    return df
+
+
+def process_transformer(df, train=True):
+    if train:
+        df = df.loc[:, ['synopsis', 'titre', 'genre']]
+    else:
+        df = df.loc[:, ['synopsis', 'titre']]
+    df = process_title_transformer(df)
+    df = process_synopsis_transformer(df)
     if train:
         df = df.loc[:, ['synopsis_use', 'titre_use', 'genre']]
     else:
